cefacd11df3af42710275961880431c5e3fccf07
JoshC-Working <josh.chau,github@gmail.com>
2023-09-17 06:46:23 +0800

    update

--- predict_data.py
+++ predict_data.py
@@ -6,43 +6,54 @@ import os
 
 
 def get_data(stock, interval, delay):
-
+    # Get historical price data form yfinance
     df = yf.download(stock)
+
+    # Take the Closing Price column only
     df = df[["Close"]]
+
+    # Take the amount of records we need for prediction
     df = df[-(interval+delay):]
     dataset = df.values
+
     return dataset
 
 
-def dataset_building(interval, delay, scaled_data):
+def prediction_set_building(interval, scaled_data):
 
-    x_evaluation = []
+    x_prediction = []
 
     for i in range(interval, len(scaled_data)):
-        x_evaluation.append(scaled_data[i-interval:i, 0])
+        # Append each of the num_of_interval of records as an element.
+        x_prediction.append(scaled_data[i-interval:i, 0])
 
-    return x_evaluation
+    return x_prediction
 
 
 def predict_data(stock, interval, delay):
 
     dataset = get_data("AAPL", interval, delay)
 
+    # Scale the target closing price
     scaler = MinMaxScaler(feature_range=(0, 1))
     scaled_data = scaler.fit_transform(dataset)
 
-    x_evaluation = dataset_building(interval, delay, scaled_data)
+    x_evaluation = prediction_set_building(interval, scaled_data)
 
     # Transform into Array type and then reshape for Keras Model
     x_evaluation = np.array(x_evaluation)
-    x_evaluation = np.reshape(
-        x_evaluation, (x_evaluation.shape[0], x_evaluation.shape[1], 1))
+    x_evaluation = x_evaluation.reshape(
+        x_evaluation.shape[0], x_evaluation.shape[1], 1)
 
+    # Build the path of the place to load the model
     root_dir = os.getcwd()
     path = os.path.join(root_dir, "Resources/"+stock+"_model")
-
     loaded_model = load_model(path)
+
+    # Do the prediction
     predictions = loaded_model.predict(x_evaluation)
+
+    # Inverse transform the outcome
     predictions = scaler.inverse_transform(predictions)
 
     return predictions
